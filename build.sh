1d73b432dacbac3348ef6e06f5aafcbbf9dcb1d8
Utkarsh-loop <utkarsh94505@gmail.com>
2026-04-13 10:46:54 +0530

    Upload missing images and auto-seed database on deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create the media products directory if it evaporates on deploy
+mkdir -p media/products
+# Seed the database with products and admin (safe since it checks if they exist)
+python init_db.py
